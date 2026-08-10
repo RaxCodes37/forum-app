dc218f9535e21c1b685be583e9436fb69574fde1
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-09 21:48:22 -0400

    Authentication mav-bar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="flex justify-center gap-20 py-5 bg-[#7e5959] rounded-b-lg">
+        <Link href="/sign-in" className="hover:underline">
+          <h2>Sign-In</h2>
+        </Link>
+        <Link href="/sign-up" className="hover:underline">
+          <h2>Sign-Up</h2>
+        </Link>
+      </nav>
+    </div>
+  )
+}
