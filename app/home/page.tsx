511b8d8512a57418d537d25b4a16bc47001595a3
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-09 23:18:02 -0400

    Added home chat

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,13 @@
+import HomeChat from "../components/pages/home-chat";
 import HomeHeader from "../components/pages/home-header";
+import SideBar from "../components/pages/side-bar";
 
 export default function Home() {
   return (
     <div>
+      <SideBar/>
       <HomeHeader/>
+      <HomeChat/>
     </div>
   )
 }
