2d9193e4dc292bfaedc984a99f1b672c0455b120
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-09 22:50:36 -0400

    rename import updates

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { signInAction } from "../api/actions";
 import SignInButton from "../components/auth/sign-in-button";
-import NavBar from "../components/navbar";
+import NavBar from "../components/auth-navbar";
 
 export default function SignInPage() {
   return (
