9f206aa59f51bdf4c5a97f0d2ead7571b238574b
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-10 01:12:04 -0400

    Minor changes + styles

--- app/components/pages/home-header.tsx
+++ app/components/pages/home-header.tsx
@@ -1,8 +1,18 @@
+"use server"
+
 import { FaPlus } from "react-icons/fa";
 import HeaderForm from "./home-header-components/form";
 import UserConfig from "./home-header-components/user-config";
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function HomeHeader() {
+export default async function HomeHeader() {
+  const session = await getSession();
+
+  if(!session) redirect("/sign-in");
+
+  const userId = session.user.id
+
   return (
     <header className="flex mt-4">
       <div className="flex justify-end items-end gap-5 w-[60%]">
@@ -14,7 +24,7 @@ export default function HomeHeader() {
       </div>
 
       <div className="flex items-end justify-end w-[30%]">
-        <UserConfig/>
+        <UserConfig userId={userId}/>
       </div>
     </header>
   );
