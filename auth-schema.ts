bce5cd6049f351477ae53c7f50cb3a940db565a0
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-12 12:07:58 -0400

    Database changes
    Planning on implementing user customization

--- auth-schema.ts
+++ auth-schema.ts
@@ -44,6 +44,7 @@ export const user = pgTable("user", {
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").default(false).notNull(),
+  messageColor: varchar("messageColor", { length: 10 }).notNull().default("#8bb3e5"),
   image: text("image"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
