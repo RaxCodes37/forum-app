2a2e6ae74e18ef71453f0bddd086bed12424e493
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-17 22:27:23 -0400

    Now forum creators can edit their descriptions
    The creators of forums can edit the description of their forums

--- app/components/pages/forum-page-components/forum-header.tsx
+++ app/components/pages/forum-page-components/forum-header.tsx
@@ -1,6 +1,10 @@
-import { Forum } from "@/utils/utils";
+import { editForumDescription, Forum } from "@/utils/utils";
 import NewPostForm from "./new-post-form";
 import { FiEdit } from "react-icons/fi";
+import { useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import { FaX } from "react-icons/fa6";
+import e from "cors";
 
 interface Props {
   forumInfo: Forum[];
@@ -9,6 +13,33 @@ interface Props {
 }
 
 export default function ForumHeader({ forumInfo, userName, userId }: Props) {
+  const [showEditButton, setShowEditButton] = useState<string>("block");
+  const [showEditForm, setShowEditForm] = useState<string>("hidden");
+  const [newDescription, setNewDescription] = useState<string>("");
+
+  const showOrHideEdit = () => {
+    if (showEditButton === "block") {
+      setShowEditButton("hidden");
+      setShowEditForm("block");
+    }
+    else {
+      setShowEditForm("hidden");
+      setShowEditButton("block");
+    }
+  };
+
+  const editDescriptionFunction = async (forumName: string) => {
+    event?.preventDefault();
+
+    if(newDescription.trim() !== ""){
+      await editForumDescription(forumName, newDescription);
+
+      setNewDescription("");
+      setShowEditForm("hidden");
+      setShowEditButton("block");
+    }
+  }
+
   return (
     <div>
       {forumInfo.map((forum) => (
@@ -21,10 +52,6 @@ export default function ForumHeader({ forumInfo, userName, userId }: Props) {
                   <p className="italic text-[#8e8c8c] mt-2">
                     This forum hasn't been given a description yet
                   </p>
-
-                  <button className="w-100">
-                    <FiEdit />
-                  </button>
                 </div>
               ) : (
                 <p className="italic text-[#8e8c8c] mt-2">
@@ -33,7 +60,44 @@ export default function ForumHeader({ forumInfo, userName, userId }: Props) {
               )}
             </div>
           ) : (
-            <p className="mt-2 text-[#8e8c8c]">{forum.forumDescription}</p>
+            <div>
+              {forum.forumCreatorName === userName ? (
+                <div className="flex flex-col items-center">
+                  <div
+                    className={`flex flex-col items-center ${showEditButton}`}
+                  >
+                    <p className="mt-2 text-[#8e8c8c]">
+                      {forum.forumDescription}
+                    </p>
+                    <button className="p-2 mt-3" onClick={showOrHideEdit}>
+                      <FiEdit />
+                    </button>
+                  </div>
+
+                  <div
+                    className={`flex justify-center items-end gap-2 ${showEditForm}`}
+                  >
+                    <form className="mt-4 flex justify-center gap-2" onSubmit={() => editDescriptionFunction(forum.forumName)}>
+                      <input
+                        type="text"
+                        placeholder={`${forum.forumDescription}`}
+                        value={newDescription}
+                        onChange={(e) => {setNewDescription(e.target.value)}}
+                      />
+                      <button type="submit" className="p-2">
+                        <FaArrowUp />
+                      </button>
+                    </form>
+
+                    <button className="p-2 danger-button" onClick={showOrHideEdit}>
+                      <FaX />
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <p className="mt-2 text-[#8e8c8c]">{forum.forumDescription}</p>
+              )}
+            </div>
           )}
           <div className="flex justify-between p-5 text-[#8e8c8c]">
             <p>
