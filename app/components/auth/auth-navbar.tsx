acf2c81ca2164adde33e40ff4167777656dffb56
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-08-18 22:48:52 -0400

    FINALLY changed navbar colors

--- app/components/auth/auth-navbar.tsx
+++ app/components/auth/auth-navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div>
-      <nav className="flex justify-center gap-20 py-5 bg-[#7e5959] rounded-b-lg animate-fade-down animate-ease-in-out">
+      <nav className="flex justify-center gap-20 py-5 rounded-b-lg animate-fade-down animate-ease-in-out" id="container">
         <Link href="/sign-in" className="hover:underline">
           <h2>Sign-In</h2>
         </Link>
